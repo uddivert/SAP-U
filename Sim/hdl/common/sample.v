c3216454ffccf27a947a8393e176b7e19799dee9
Uddhav Swami <71356488+uddivert@users.noreply.github.com>
2024-10-14 15:56:45 -0500

    Create sample.v

---
+++ Sim/hdl/common/sample.v
@@ -0,0 +1,16 @@
+`default_nettype none
+module d_flip_flop (
+    input             clk  ,     // Input: clock
+    input       data,  // Input: data for the signal
+    output   reg q,     // Output: Q
+    output wire q_not  // Output: Not Q
+);
+
+  // On the rising edge of the clock, the flip-flop captures the input data
+  always @(posedge clk)   begin
+    q <= data;
+  end
+
+  // Not Q is simply the inverse of Q
+  assign    q_not = ~q;
+endmodule
