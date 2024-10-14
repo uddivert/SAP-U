6de80328bdfa4abb253bb847e25f77a4189c321a
Uddhav Swami <71356488+uddivert@users.noreply.github.com>
2024-10-14 15:39:13 -0500

    Create Sample.v

---
+++ Sample.v
@@ -0,0 +1,13 @@
+`default_nettype none
+module d_latch (
+    input             enable,  // Input: Enables latch to be on
+  input       data,    // Input: data for the signal
+    output wire q,       // Output: Q
+    output wire q_not    // Output: Not Q
+);
+
+  // D latch using NOR gates and AND gates
+    assign q =            ~((enable & ~data) | q_not);
+  assign q_not = ~((enable & data) | q);
+
+endmodule
