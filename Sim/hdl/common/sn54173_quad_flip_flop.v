a5833f4574f602c4d2418fd3ef0c585fba70ad95
Uddhav Swami <71356488+uddivert@users.noreply.github.com>
2024-10-17 10:50:40 -0500

    Update sn54173_quad_flip_flop.v
    Cleaned comments in code

--- Sim/hdl/common/sn54173_quad_flip_flop.v
+++ Sim/hdl/common/sn54173_quad_flip_flop.v
@@ -5,9 +5,9 @@ module sn54173_quad_flip_flop (
     input  wire n,
     input  wire g1,
     input  wire g2,
-    input  wire [3:0] data,
     input  wire clr,
     input  wire clk,
+    input  wire [3:0] data,
     output wire [3:0] q 
 );
 
@@ -16,10 +16,12 @@ module sn54173_quad_flip_flop (
     wire [3:0] not_q;
     wire [3:0] flop_data;
 
-    // Adjust logic of data input and load control
+    // If  g1 and g2 BOTH low and load is enabled, data is loaded into d flipflop.
+    // else value stays with current value of Q
+    // M & N must both be low for output to be set
     for (i = 0; i < 4; i = i + 1) begin
-        assign flop_data[i] = ( (~load & internal_q[i]) | (load & data[i]) & ~g1 & ~g2); // Data assigned if g1 and g2 both low, and load is enabled
-        assign q = internal_q[i] & ~m & ~n;  // Capture internal_q on clock edge. Only output if m and n are BOTH low
+        assign flop_data[i] = ( (~load & internal_q[i]) | (load & data[i]) & ~g1 & ~g2); 
+        assign q = internal_q[i] & ~m & ~n;
     end 
 
     d_flip_flop d_flip_flop1 (
