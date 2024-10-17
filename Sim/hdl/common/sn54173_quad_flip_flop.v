42cfd837295241dd3f88085ce3b450529704cc66
Uddhav Swami <71356488+uddivert@users.noreply.github.com>
2024-10-17 10:41:10 -0500

    Checkpoint: more complete sn54173 model

--- Sim/hdl/common/sn54173_quad_flip_flop.v
+++ Sim/hdl/common/sn54173_quad_flip_flop.v
@@ -1,24 +1,54 @@
 `default_nettype none
 module sn54173_quad_flip_flop (
-    input  wire load,
-    input  wire data,
-    input  wire reset,
+    // m and n both need to be low for output to be enabled
+    input  wire m,
+    input  wire n,
+    input  wire g1,
+    input  wire g2,
+    input  wire [3:0] data,
+    input  wire clr,
     input  wire clk,
-    output wire q
+    output wire [3:0] q 
 );
 
-    wire internal_q, not_q, flop_data;
+    // internal signals
+    wire [3:0] internal_q;
+    wire [3:0] not_q;
+    wire [3:0] flop_data;
 
     // Adjust logic of data input and load control
-    assign flop_data = (~load & internal_q) | (load & data);
-    assign q = internal_q;  // Capture internal_q on clock edge
+    for (i = 0; i < 4; i = i + 1) begin
+        assign flop_data[i] = ( (~load & internal_q[i]) | (load & data[i]) & ~g1 & ~g2); // Data assigned if g1 and g2 both low, and load is enabled
+        assign q = internal_q[i] & ~m & ~n;  // Capture internal_q on clock edge. Only output if m and n are BOTH low
+    end 
 
     d_flip_flop d_flip_flop1 (
         .clk(clk),
-        .data(flop_data),
-        .reset(reset),
-        .q(internal_q),
-        .q_not(not_q)
+        .data(flop_data[0]),
+        .reset(clear),
+        .q(internal_q)[0],
+        .q_not(not_q[0])
     );
 
+    d_flip_flop d_flip_flop2 (
+        .clk(clk),
+        .data(flop_data[1]),
+        .reset(clear),
+        .q(internal_q)[1],
+        .q_not(not_q[1])
+    );
+    d_flip_flop d_flip_flop3 (
+        .clk(clk),
+        .data(flop_data[2]),
+        .reset(clear),
+        .q(internal_q)[2],
+        .q_not(not_q[2])
+    );
+    d_flip_flop d_flip_flop4 (
+        .clk(clk),
+        .data(flop_data[3]),
+        .reset(clear),
+        .q(internal_q)[3],
+        .q_not(not_q[3])
+    );
 endmodule
