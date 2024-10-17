92e38d530bb7b4ce3ef5e7e8a3635c0f876a13b7
Uddhav Swami <71356488+uddivert@users.noreply.github.com>
2024-10-17 00:15:43 -0500

    Update Sim/hdl/common/sn54173_quad_flip_flop.v
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Sim/hdl/common/sn54173_quad_flip_flop.v
+++ Sim/hdl/common/sn54173_quad_flip_flop.v
@@ -13,12 +13,12 @@ module sn54173_quad_flip_flop (
   assign flop_data = (~load & internal_q) | (load & data);
   assign q = internal_q;  // Capture internal_q on clock edge
 
-d_flip_flop d_flip_flop1 (
-  .clk(clk),
-  .data(flop_data),
-  .reset(reset),
-  .q(internal_q),
-  .q_not(not_q)
-);
+  d_flip_flop d_flip_flop1 (
+      .clk(clk),
+      .data(flop_data),
+      .reset(reset),
+      .q(internal_q),
+      .q_not(not_q)
+  );
 
 endmodule
