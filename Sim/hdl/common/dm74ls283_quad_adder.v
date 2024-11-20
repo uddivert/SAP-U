09b2966cce2601dd74a5b88fc36c023239a73761
Uddhav Swami <71356488+uddivert@users.noreply.github.com>
2024-11-20 13:35:40 -0600

    added templates for the dm74ls283

---
+++ Sim/hdl/common/dm74ls283_quad_adder.v
@@ -0,0 +1,11 @@
+module dm74ls283_quad_adder(
+    input wire [1:4] a, // input b
+    input wire [1:4] b, // input a
+    input wire c0, // carry in
+    output wire [1:4] s1, // sum
+    output reg c4 // carry out
+);
+
+// todo
+
+endmodule
---
+++ Sim/test/dm74ls283_quad_adder_tb.v
@@ -0,0 +1,11 @@
+module dm74ls283_quad_adder_tb();
+
+input reg [1:4] a, // input b
+input reg [1:4] b, // input a
+input reg c0, // carry in
+output reg [1:4] s1, // sum
+output wire c4 // carry out
+
+// todo
+
+endmodule
