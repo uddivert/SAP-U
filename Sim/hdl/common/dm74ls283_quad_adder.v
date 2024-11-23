c8678c2c4c4d3890f383a535976b925191145a8a
uddivert <uddhavswami@gmail.com>
2024-11-23 00:04:12 -0600

    added adder and test code

--- Sim/hdl/common/dm74ls283_quad_adder.v
+++ Sim/hdl/common/dm74ls283_quad_adder.v
@@ -1,11 +1,43 @@
 module dm74ls283_quad_adder(
-    input wire [1:4] a, // input b
-    input wire [1:4] b, // input a
-    input wire c0, // carry in
-    output wire [1:4] s1, // sum
-    output reg c4 // carry out
+    input wire [4:1] a, // input b
+    input wire [4:1] b, // input a
+    input wire cin, // carry in
+    output wire [4:1] s1, // sum
+    output wire cout // carry out
 );
 
-// todo
+wire [4:1] gen; // generate signal
+wire [4:1] prop; // propagate signal
+wire [4:0] carry; // carry signals
+
+// intial carry in
+assign carry[0] = cin;
+
+// create generate and propagation signals
+assign gen = a & b;
+assign prop = a | b; 
+
+// carry calculation
+assign carry[1] = gen[1] | (prop[1] & carry[0]);
+assign carry[2] = gen[2] | (prop[2] & carry[1]);
+assign carry[3] = gen[3] | (prop[3] & carry[2]);
+assign carry[4] = gen[4] | (prop[4] & carry[3]);
+
+// Use `generate` to instantiate the `full_adder` modules
+genvar i;
+generate
+    for (i = 1; i <= 4; i = i + 1) begin : adder_chain
+        full_adder full_adder_inst (
+            .a(a[i]),
+            .b(b[i]),
+            .cin(carry[i-1]),
+            .s(s1[i]),
+            .cout() // carry-out is handled separately by `c` array
+        );
+    end
+endgenerate
+
+// Final carry out
+assign cout = carry[4];
 
 endmodule
--- Sim/test/dm74ls283_quad_adder_tb.v
+++ Sim/test/dm74ls283_quad_adder_tb.v
@@ -1,11 +1,45 @@
+`timescale 1ns / 1ps
+
 module dm74ls283_quad_adder_tb();
+reg [4:1] a; // input b
+reg [4:1] b; // input a
+reg cin; // carry in
+wire [4:1] s1; // sum
+wire cout; // carry out
 
-input reg [1:4] a, // input b
-input reg [1:4] b, // input a
-input reg c0, // carry in
-output reg [1:4] s1, // sum
-output wire c4 // carry out
+dm74ls283_quad_adder uut(
+    .a(a),
+    .b(b),
+    .cin(cin),
+    .s1(s1),
+    .cout(cout)
+);
 
-// todo
+  // Testbench stimulus
+  initial begin
+    $dumpfile("./simulation/testbench_master.vcd");  // VCD file for waveform generation
+    $dumpvars(0, dm74ls283_quad_adder_tb);
 
+    // setup conditions
+    a = 4'b0000;
+    b = 4'b0000;
+    cin = 0;
+    #10
+
+    a = 4'b1000;
+    b = 4'b0000;
+    cin = 0;
+    #10
+    
+    a = 4'b1000;
+    b = 4'b0000;
+    cin = 1;
+    #10
+
+    a = 4'b1111;
+    b = 4'b1111;
+    cin = 1;
+    #10
+    $finish;
+  end
 endmodule
