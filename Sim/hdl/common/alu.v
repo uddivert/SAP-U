256d3e3d463beeb40597e8c5b723b129932d09a2
uddivert <uddhavswami@gmail.com>
2024-12-11 21:38:49 -0600

    fixed flags register

--- Sim/hdl/common/alu.v
+++ Sim/hdl/common/alu.v
@@ -6,7 +6,9 @@ module alu(
     input wire subtract,
     input wire flag_fi, // controls if flag is holding value or updating
     input wire flagclr, // clears flags
-    output wire [7:0] result
+    input wire clk,
+    output wire [7:0] result,
+    output wire [1:0] flag_out
 );
 
 /*
@@ -38,7 +40,6 @@ dm74ls283_quad_adder adder2(
     .cout(external_carryout) // carry out ignored
 );
 
-wire fi;
 wire [3:0] intermediate_zero_data;
 wire [1:0] flag_data;
 
@@ -46,26 +47,18 @@ wire [1:0] flag_data;
 assign flag_data[0] = external_carryout;
 
 // compute zero flag
-assign intermediate_zero_data[0] = internal_result[0] ^ internal_result[1];
-assign intermediate_zero_data[1] = internal_result[2] ^ internal_result[3];
-assign intermediate_zero_data[2] = internal_result[4] ^ internal_result[5];
-assign intermediate_zero_data[3] = internal_result[6] ^ internal_result[7];
-assign flag_data[1] = intermediate_zero_data[0] & intermediate_zero_data[1] &
-                      intermediate_zero_data[2] & intermediate_zero_data[3];
-
-assign {m, n} = 0;  // enable output always on
+assign flag_data[1] = ~|internal_result; // Simplifies zero flag logic
 assign result = enable ? 8'b0 :internal_result; // assign result dependent on enable
-assign flag_fi = fi; // assign flag to internal flag 
 
 // handle flags
 sn54173_quad_flip_flop flags_reg (
-    .m(m),
-    .n(n),
-    .g1(fi),
-    .g2(fi),
+    .m(1'b0),
+    .n(1'b0),
+    .g1(flag_fi),
+    .g2(flag_fi),
     .clr(flagclr),
     .clk(clk),
     .data(flag_data),
-    .q(flag_data)
-)
+    .q( flag_out)
+);
 endmodule
